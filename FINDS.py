e6231d0258a9ab55af226fef031fc953ed9ae294
Channaveereshmeti2004 <167081535+Channaveereshmeti2004@users.noreply.github.com>
2025-03-21 15:29:16 +0530

    FINDS.py

--- FINDS.py
+++ FINDS.py
@@ -25,3 +25,51 @@ for i in range(0,len(a)):
 print("The maximally specific hypothesis is ",hypothesis)
 
 
+
+
+''' ********************* OUTPUT ***********************'''
+ The given training data set 
+
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+The initial values of hypothesis 
+['0', '0', '0', '0', '0', '0']
+For training instance no: 0  the hypothesis is  ['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same']
+For training instance no: 1  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For training instance no: 2  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For training instance no: 3  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 4  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 5  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 6  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 7  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 8  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 9  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 10  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 11  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 12  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 13  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 14  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 15  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 16  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 17  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 18  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For training instance no: 19  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+The maximally specific hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
